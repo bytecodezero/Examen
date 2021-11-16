c1a4780834d00b268da850c81aa4bbb905f3c775
bytecodezero <dark_1895@live.com.mx>
2021-11-15 19:52:39 -0600

    conversion fechas del resultado del ws

--- src/programaCuatro/principal.java
+++ src/programaCuatro/principal.java
@@ -20,6 +20,7 @@ public class principal {
 		return cliente.target(REST_URI).request(MediaType.APPLICATION_JSON).get(Tiempo.class);
 		
 	}
+	
 
 	public static void main(String[] args) {
 
@@ -30,12 +31,32 @@ public class principal {
 			        	   
 			        		Tiempo tiempo = new Tiempo();
 			        		tiempo = getJsonTime();
-			         
-	
-			        		System.out.println(tiempo.getDate());
-			        		System.out.println(tiempo.getMilliseconds_since_epoch());
-			        		System.out.println(tiempo.getTime());
 			        		
+			        		Date date = null;
+			        		DateFormat formato = null;
+			        		
+			        		formato = new SimpleDateFormat("mm-dd-yyyy");
+			        		     			    
+			        		date = formato.parse(tiempo.getDate());
+			        	    formato = new SimpleDateFormat("yyyy-mm-dd");
+			        		String fecha1Conversion = formato.format(date);
+			        		
+			        		date = new Date(Long.parseLong(tiempo.getMilliseconds_since_epoch()));
+			                formato = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+			                String fecha2Conversion = formato.format((Long.parseLong(tiempo.getMilliseconds_since_epoch())));		           
+			                formato.setTimeZone(TimeZone.getTimeZone("America/Mexico_City"));
+			                fecha2Conversion = formato.format(date);
+
+			                formato = new SimpleDateFormat("HH:mm:ss");
+			                String fecha3Conversion = formato.format(formato.parse(tiempo.getTime()));
+			            	
+			                System.out.println(tiempo.getDate());  
+			            	System.out.println(tiempo.getMilliseconds_since_epoch());
+			                System.out.println(tiempo.getTime());
+			                
+			            	System.out.println("Conversion 1:" + fecha1Conversion);
+			            	System.out.println("Conversion 2: " + fecha2Conversion);
+			                System.out.println("Conversion 3: " + fecha3Conversion);
 			        	
 			        	     Thread.sleep(5000);
 			            }
@@ -43,9 +64,11 @@ public class principal {
 			        } catch (InterruptedException e) {
 			            // TODO Auto-generated catch block
 			            e.printStackTrace();
-			        }
+			        } catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 			    }
 			}).start();
-
 	}
 }
